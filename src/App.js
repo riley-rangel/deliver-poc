f3378262ff267a611587a0b673928081db653a21
Riley Rangel <rrangel91@gmail.com>
2020-04-22 15:17:40 -0700

    add rebass

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import {
+  Box, Heading, Text
+} from 'rebass';
 
 function App() {
   return (
-    <div>empty test</div>
+    <Box>
+      <Heading as="h1">Heading</Heading>
+      <Text>Here's some text.</Text>
+      <Text>Beautiful...</Text>
+    </Box>
   );
 }
 
