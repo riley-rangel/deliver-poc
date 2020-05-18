f8217ce6848296ab580bd46cb44f2e9b5fbe25de
Riley Rangel <rrangel91@gmail.com>
2020-05-18 16:53:54 -0700

    add canvas and control bar to app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import React from 'react';
-import {
-  Box, Heading, Text
-} from 'rebass';
+
+import { Box } from 'rebass';
+import { Canvas } from './components/Canvas';
+import { ControlBar } from './components/ControlBar';
+
+import './App.css';
 
 function App() {
   return (
-    <Box>
-      <Heading as="h1">Heading</Heading>
-      <Text>Here's some text.</Text>
-      <Text>Beautiful...</Text>
-    </Box>
+    <>
+      <Canvas canvasId="canvas-one">
+        <Box height="200px" width="100%" sx={{ border: '1px solid black' }}>
+          <Box height="50px" width="50px" sx={{ border: '1px solid black' }}>
+            Box
+          </Box>
+          <Box height="50px" width="50px" sx={{ border: '1px solid black' }}>
+            Box
+          </Box>
+          <Box height="50px" width="50px" sx={{ border: '1px solid black' }}>
+            Box
+          </Box>
+        </Box>
+      </Canvas>
+
+      <ControlBar>
+        <Box>Add Element</Box>
+      </ControlBar>
+    </>
   );
 }
 
