24f8f4845e29a032e8916b53d5b37e0910682763
Riley Rangel <rrangel91@gmail.com>
2020-05-18 16:53:43 -0700

    define control bar

---
+++ src/components/ControlBar/ControlBar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Box } from 'rebass';
+
+/**
+ * UI component responsible for visual representation of the control bar API.
+ * */
+const ControlBar = ({ children }) => {
+  return (
+    <Box
+      display="flex"
+      height="90px"
+      width="100%"
+      p={2}
+      sx={{ position: 'fixed', bottom: 0 }}
+    >
+      <Box height="100%" sx={{ border: '2px solid black' }}>
+        {children}
+      </Box>
+    </Box>
+  );
+};
+
+export default ControlBar;
